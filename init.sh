e30d1907419bf762fbbaf95f290be0fe0dffced0
prozhou <zhoushengzheng@gmail.com>
2017-12-07 12:30:04 +0800

    YCM for ubuntu16.04

--- init.sh
+++ init.sh
@@ -51,14 +51,24 @@ fi
 #-------------------------------------------------------------------------------
 # compile ycm_core
 #-------------------------------------------------------------------------------
+#系统名称
+com_codename=$(cat /etc/lsb-release 2>/dev/null|awk -F "=" ' $1 == "DISTRIB_ID" {print $2}'|tr '[A-Z]' '[a-z]')
 
 if [ ! -f ~/.vim/bundle/YouCompleteMe/third_party/ycmd/ycm_core.so ] ; then
     cd ~
     mkdir ycm_build
     cd ycm_build
-    wget http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-x86_64-linux-gnu-debian8.tar.xz
-    tar xJvf clang+llvm-5.0.0-x86_64-linux-gnu-debian8.tar.xz
-    cmake -G "Unix Makefiles"  -DPATH_TO_LLVM_ROOT=./clang+llvm-5.0.0-x86_64-linux-gnu-debian8    . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+
+    if [ "$com_codename"x = "deepin"x ] ; then
+        wget http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-x86_64-linux-gnu-debian8.tar.xz
+        tar xJvf clang+llvm-5.0.0-x86_64-linux-gnu-debian8.tar.xz
+        cmake -G "Unix Makefiles"  -DPATH_TO_LLVM_ROOT=./clang+llvm-5.0.0-x86_64-linux-gnu-debian8    . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+    elif ["$com_codename"x = "ubuntu"x ] ; then
+        wget http://releases.llvm.org/5.0.0/clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
+        tar xJvf clang+llvm-5.0.0-linux-x86_64-ubuntu16.04.tar.xz
+        cmake -G "Unix Makefiles"  -DPATH_TO_LLVM_ROOT=./clang+llvm-5.0.0-linux-x86_64-ubuntu16.04    . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+    fi
+
     cmake --build . --target ycm_core --config Release
     cd ..
     rm -rf ycm_build
