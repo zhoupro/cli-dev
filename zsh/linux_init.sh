87b1b629a218e2b26f526fdfec412ee1f38449c3
prozhou <zhoushengzheng@gmail.com>
2018-04-05 06:44:49 +0800

    add linux zsh

---
+++ zsh/linux_init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -
+#===============================================================================
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2018/03/24 18时08分31秒
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
+sudo apt-get install zsh -y
+[ ! -d ~/.oh-my-zsh ] && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
