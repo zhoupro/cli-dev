33b118b74a2db5ce6e62a655df50c28aa08d4f56
zhoushengzheng <zhoushengzheng@gmail.com>
2018-09-08 06:21:31 +0800

    add cmake

--- neovim/linux_init.sh
+++ neovim/linux_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 set -o nounset                                  # Treat unset variables as an error
-apt-get install -y --no-install-recommends git unzip wget curl python-dev cscope ctags
+apt-get install -y --no-install-recommends git unzip wget curl python-dev cscope ctags cmake
 #读取参数
 # shellcheck disable=SC1091
 apt-get  install -y  neovim python3-pip
