69458504d8c53c6688cf1f0dae334a5236351000
zhoushengzheng <zhoushengzheng@gmail.com>
2018-09-26 09:52:25 +0800

    add custom env

--- zsh/linux_init.sh
+++ zsh/linux_init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -
 #===============================================================================
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
+#        AUTHOR:  prozhou 
 #       CREATED: 2018/03/24 18时08分31秒
-#      REVISION:  ---
 #===============================================================================
 
 set -o nounset                                  # Treat unset variables as an error
 apt-get install zsh language-pack-zh-hans language-pack-zh-hans-base -y
 sudo usermod -s /bin/zsh root
-[ ! -d ~/.oh-my-zsh ] && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-echo 'export LC_ALL=zh_CN.UTF-8' >> ~/.zshrc
-echo 'export LANG=zh_CN.UTF-8' >> ~/.zshrc
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo 'export LC_ALL=zh_CN.UTF-8' >> ~/.env
+echo 'export LANG=zh_CN.UTF-8' >> ~/.env
+echo "export PATH=\$PATH:$HOME/.composer/vendor/bin" > $HOME/.env
+echo 'if [ -f ~/.env ];then; source ~/.env;fi' >> ~/.zshrc
 
