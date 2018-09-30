a8617209df190d6c4c1fa740fd252e483a1b8207
zhoushengzheng <zhoushengzheng@gmail.com>
2018-09-30 17:30:23 +0800

    fix

--- zsh/linux_init.sh
+++ zsh/linux_init.sh
@@ -7,7 +7,8 @@
 set -o nounset                                  # Treat unset variables as an error
 apt-get install gawk curl  zsh language-pack-zh-hans language-pack-zh-hans-base -y
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+[ ! -d $HOME/.zplug ] && curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 sudo usermod -s /bin/zsh root
 
 ! (grep -F 'LC_ALL' ~/.env >/dev/null )  && \
@@ -19,7 +20,7 @@ echo "export PATH=\$PATH:$HOME/.composer/vendor/bin" >> "$HOME/.env"
 ! (grep -F '.env' ~/.zshrc >/dev/null )  && \
     echo 'if [ -f ~/.env ];then; source ~/.env;fi' >> ~/.zshrc
 
-! (grep -F 'zsh-syntax-highlighting' ~/.cus_zshrc )  && \
+! (grep -F 'zsh-syntax-highlighting' ~/.cus_zshrc >/dev/null )  && \
 cat >> ~/.cus_zshrc <<END
 export ZPLUG_HOME=~/.zplug
 source \$ZPLUG_HOME/init.zsh
