66098e995beb0de65dd79ad1ba8058fd0cf53820
zhoushengzheng <zhoushengzheng@gmail.com>
2018-10-19 22:22:19 +0800

    genurl quiet

--- neovim/genUrl.sh
+++ neovim/genUrl.sh
@@ -2,24 +2,34 @@
 # check file
 [ ! -f "$1" ] &&
     exit 0
-git_path="~/.local/share/nvim/plugged/images"
+git_path="$HOME/.local/share/nvim/plugged/"
+git_repo="git@github.com:zhoupro/images.git"
+repo_name="images"
 github_prefix="https://raw.githubusercontent.com/zhoupro/images/master"
 old_path=$(pwd)
 cur_date=$(date +"%Y%m%d")
 file_name=$(basename "$1")
-cd "$git_path" && \
+
+if [  ! -d "$git_path/$repo_name/.git" ]
+then
+    cd $git_path  
+    git clone --quiet $git_repo  
+    cd $old_path
+fi
+cd "$git_path/$repo_name" && \
 ls   | grep -v $cur_date | xargs rm -rf &&\
 cd "$old_path"
 
-if [ -f "$git_path/$cur_date/$file_name" ]
+if [ -f "$git_path/$repo_name/$cur_date/$file_name" ]
 then 
     echo "$github_prefix/$cur_date/$file_name"
     exit 0
 fi
-mkdir -p $git_path/$cur_date
-cp $1 $git_path/$cur_date
-cd $git_path && \
-git add "$cur_date" &&\
-git commit -m "$cur_date file" &&\
-git push origin master &&\
+mkdir -p $git_path/$repo_name/$cur_date
+cp $1 $git_path/$repo_name/$cur_date
+cd $git_path/$repo_name && \
+git add  "$cur_date" > /dev/null  &&\
+git commit  -m "$cur_date $file_name" >/dev/null  &&\
+git push --quiet origin master  &&\
 cd $old_path
+echo "$github_prefix/$cur_date/$file_name"
