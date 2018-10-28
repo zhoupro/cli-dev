0936884e5374b40b33e2ef04bc07ef0249959f0d
prozhou <zhoushengzheng@gmail.com>
2018-10-28 01:34:27 +0000

    add autojump

--- zsh/linux_init.sh
+++ zsh/linux_init.sh
@@ -52,3 +52,7 @@ END
     git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt" && \
     ln -s "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme" &&\
     sed -i 's#ZSH_THEME="robbyrussell"#ZSH_THEME="spaceship"#g' ~/.zshrc
+
+#autojump
+! (grep -F 'autojump' ~/.zshrc &>/dev/null )  && \
+    sed -in '/^plugins=/a autojump' ~/.zshrc
