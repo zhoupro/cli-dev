b8c07d34bb8bc056551328c4c7ff4ec853bd209f
prozhou <zhoushengzheng@gmail.com>
2018-11-28 03:35:56 +0000

    zsh vim mode and change theme to avit

--- zsh/linux_init.sh
+++ zsh/linux_init.sh
@@ -53,3 +53,31 @@ END
 #env
 ! (grep -F '.env' ~/.zshrc &>/dev/null )  && \
     echo 'if [ -f ~/.env ];then; source ~/.env;fi' >> ~/.zshrc
+
+#themes
+! (grep -F 'spaceship' ~/.zshrc &>/dev/null )  && \
+    sed -i 's#ZSH_THEME="robbyrussell"#ZSH_THEME="avit"#g' ~/.zshrc
+
+
+! (grep -F 'history-incremental-search-backward' ~/.cus_zshrc &>/dev/null )  && \
+cat >> ~/.cus_zshrc <<END
+
+bindkey -v
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{\$fg_bold[yellow]%} [% NORMAL]%  %{\$reset_color%}"
+    RPS1="\${\${KEYMAP/vicmd/\$VIM_PROMPT}/(main|viins)/} \$EPS1"
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+export KEYTIMEOUT=1
+END
