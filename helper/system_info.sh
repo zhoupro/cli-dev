1db61584ad1e076cafede0c31981bd5890cff57d
prozhou <zhoushengzheng@gmail.com>
2018-12-13 17:04:59 +0800

    system judge

--- helper/system_info.sh
+++ helper/system_info.sh
@@ -3,6 +3,6 @@ if [ "$(uname)" == "Darwin" ]; then
     sys_os="mac"
     sys_version=$(sw_vers | grep ProductVersion | awk  '{print $2}')
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sys_os=$(cat /etc/lsb-release 2>/dev/null|awk -F "=" ' $1 == "DISTRIB_ID" {print $2}'|tr '[A-Z]' '[a-z]')
-    sys_version=$(cat /etc/lsb-release 2>/dev/null|awk -F "=" ' $1 == "DISTRIB_RELEASE" {print $2}')
+    sys_version=$(cat /etc/os-release | grep ^VERSION_ID= | awk -F '=' '{print $2}' |sed -e 's/"//g')
+    sys_os=$(cat /etc/os-release | grep ^ID= | awk -F '=' '{print $2}' |sed -e 's/"//g')
 fi
