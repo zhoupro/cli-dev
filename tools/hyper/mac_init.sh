27f0d0680a3d17c11b20032067e39d67ac653a99
prozhou <zhoushengzheng@gmail.com>
2018-12-26 15:00:45 +0800

    add proxy

--- tools/hyper/mac_init.sh
+++ tools/hyper/mac_init.sh
@@ -4,8 +4,9 @@ cp tools/hyper/hyper.js ~/.hyper.js
 if [ -d "/Applications/Hyper.app" ];then
     return
 fi
-wget "https://github.com/zeit/hyper/releases/download/2.1.0-canary.4/Hyper-2.1.0-canary.4.dmg"
-sudo hdiutil attach -quiet "Hyper-2.1.0-canary.4.dmg"
-sudo cp -r "/Volumes/Hyper 2.1.0-canary.4/Hyper.app" /Applications
-sudo hdiutil detach  "/Volumes/Hyper 2.1.0-canary.4"
-rm -rf "Hyper-2.1.0-canary.4.dmg"
+
+wget "https://github.com/zeit/hyper/releases/download/2.1.0/Hyper-2.1.0.dmg"
+sudo hdiutil attach -quiet "Hyper-2.1.0.dmg"
+sudo cp -r "/Volumes/Hyper 2.1.0/Hyper.app" /Applications
+sudo hdiutil detach  "/Volumes/Hyper 2.1.0"
+rm -rf "Hyper-2.1.0.dmg"
--- tools/pxy/pxy.sh
+++ tools/pxy/pxy.sh
@@ -2,7 +2,14 @@
 export http_proxy=""
 export https_proxy=""
 
-if  nmap localhost -p 8087 | grep open >/dev/null;then
+if nmap localhost -p 8787 | grep open >/dev/null;then
+    export http_proxy=http://localhost:8787
+    export https_proxy=http://localhost:8787
+elif nmap localhost -p 8787 | grep open >/dev/null;then
+    ip=$(/sbin/ip route|awk '/default/ { print $3 }')
+    export http_proxy=http://$ip:8787
+    export https_proxy=http://$ip:8787
+elif  nmap localhost -p 8087 | grep open >/dev/null;then
     export http_proxy=http://localhost:8087
     export https_proxy=http://localhost:8087
     curl http://localhost:8085/module/gae_proxy/control/download_cert |sudo tee  --append /usr/local/share/ca-certificates/goagent.crt
@@ -19,11 +26,9 @@ else
     export http_proxy=""
     export https_proxy=""
 fi
-echo $http_proxy
 starttime=$(date +%s)
 #执行程序
 "$@"
-
 endtime=$(date +%s)
 echo "use time： "$((endtime-starttime))"s"
 unset http_proxy
