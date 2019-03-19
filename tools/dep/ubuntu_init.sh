59f8ddb68704bf22dc4e31bd28ecec70fece7a44
prozhou <zhoushengzheng@gmail.com>
2019-03-19 11:15:01 +0800

    change

--- tools/dep/ubuntu_init.sh
+++ tools/dep/ubuntu_init.sh
@@ -7,7 +7,7 @@ apt-get install -y gawk curl  zsh language-pack-zh-hans language-pack-zh-hans-ba
     git unzip wget curl python-dev cscope  cmake gdb lsof \
     python3-pip python-pip shellcheck libtool-bin gettext sudo \
     automake m4 autoconf libtool build-essential cmake pkg-config lua5.2 wamerican \
-    autojump valgrind nmap iproute2
+    autojump valgrind nmap iproute2 net-tools
 
 apt-get remove -y neovim exuberant-ctags
 
