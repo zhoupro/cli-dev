179a267947137b881aac79fa2ebc9d52f39c51c8
prozhou <zhoushengzheng@gmail.com>
2019-03-24 22:47:32 +0800

    zsh

--- tools/zsh/ubuntu_init.sh
+++ tools/zsh/ubuntu_init.sh
@@ -8,7 +8,7 @@ set -o nounset                                  # Treat unset variables as an er
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -| sed  's/env zsh -l//g')"
 
 if [ ! -d "$HOME/.zplug" ];then
-    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+    ( curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh )
     sudo usermod -s /bin/zsh root
 fi
 ! (grep -F 'zsh-autosuggestions' ~/.zshrc &>/dev/null )  && \
