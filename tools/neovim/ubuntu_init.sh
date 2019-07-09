719ec1e668c5694da764e97becc262b3c1bfcd13
prozhou <zhoushengzheng@gmail.com>
2019-07-09 17:46:15 +0800

    coc default setting

--- tools/neovim/ubuntu_init.sh
+++ tools/neovim/ubuntu_init.sh
@@ -288,3 +288,43 @@ if [ "Y$OPT_DICT" == "Yyes" ];then
     nmap  <leader>w :call QuerySel() <CR>
 END
 fi
+
+#coc setting
+! ( grep -F "show_documentation" ~/.config/nvim/init.vim ) && \
+cat >> ~/.config/nvim/init.vim <<END
+" if hidden is not set, TextEdit might fail.
+set hidden
+
+" Some servers have issues with backup files, see #649
+set nobackup
+set nowritebackup
+
+" Better display for messages
+set cmdheight=2
+
+" You will have bad experience for diagnostic messages when it's default 4000.
+set updatetime=300
+
+" don't give |ins-completion-menu| messages.
+set shortmess+=c
+
+" always show signcolumns
+set signcolumn=yes
+" Remap keys for gotos
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gy <Plug>(coc-type-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+
+" Use K to show documentation in preview window
+nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+function! s:show_documentation()
+  if (index(['vim','help'], &filetype) >= 0)
+    execute 'h '.expand('<cword>')
+  else
+    call CocAction('doHover')
+  endif
+endfunction
+nmap <leader>rn <Plug>(coc-rename)
+END
